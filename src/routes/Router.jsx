9c767ea8301d7daa8450eb05387bc88eab6aaac2
Minhajul Islam <mislam2330718@bscse.uiu.ac.bd>
2025-11-13 23:07:15 +0600

    Sheeter Alo, initial project created in first commit.

---
+++ src/routes/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from 'react-router';
+
+const Router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Home</div>,
+    },
+]);
+
+export default Router;
