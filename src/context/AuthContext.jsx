8b565a4ae4a5f5ddc4672492572813ebdc62b778
Minhajul Islam <mislam2330718@bscse.uiu.ac.bd>
2025-11-17 21:26:49 +0600

    index.css bg color changed

--- src/context/AuthContext.jsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import AuthProvider, { AuthContext } from '../provider/AuthProvider';
-
-// eslint-disable-next-line react-refresh/only-export-components
-export const useAuth = () => useContext(AuthContext);
-
-export { AuthContext };
-
-export default AuthProvider;
